fe94ad5ddb8703cefa62690325b65e67c94c0c37
Dennis Jian <dennis.jian01@gmail.com>
2024-12-19 12:34:57 +1100

    20241219_02

--- UI_main.go
+++ UI_main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -25,30 +25,50 @@ func makeUI(w fyne.Window, a fyne.App) {
 	// ToolbarContainer
 	ToolbarWidget := widget.NewToolbar(
 		widget.NewToolbarSpacer(),
-		widget.NewToolbarAction(theme.ColorPaletteIcon(), func() {
+		widget.NewToolbarAction(theme.RadioButtonCheckedIcon(), func() {
 			// Toggle between light and dark theme
 			if currentTheme == "light" {
-				fmt.Println("set dark theme")
 				currentTheme = "dark"
 				fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
 			} else {
-				fmt.Println("set light theme")
 				currentTheme = "light"
 				fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
 			}
 		}),
-		// widget.NewToolbarAction(theme.ContentCutIcon(), func() {}),
+		widget.NewToolbarAction(theme.HelpIcon(), func() {
+			// Create about dialog with left-aligned text
+			aboutContent := widget.NewLabel(
+				"Version: 1.0.0\n" +
+					"Developed By: Dennis Jian\n" +
+					"Project Home: https://github.com/djian01/nt_gui\n\n")
+			aboutContent.Alignment = fyne.TextAlignLeading // Left alignment
+
+			aboutDialog := dialog.NewCustom(
+				"About NT (Net-Test) GUI",
+				"OK",
+				aboutContent,
+				w)
+			aboutDialog.Resize(fyne.NewSize(500, 130))
+			aboutDialog.Show()
+		}),
 	)
 
 	ToolbarContainer := container.New(layout.NewVBoxLayout(), ToolbarWidget)
 
+	// Create resource from SVG file
+	icmpIcon := theme.NewThemedResource(resourceIcmpIconSvg)
+	tcpIcon := theme.NewThemedResource(resourceTcpIconSvg)
+	dnsIcon := theme.NewThemedResource(resourceDnsIconSvg)
+	httpIcon := theme.NewThemedResource(resourceHttpIconSvg)
+	analyIcon := theme.NewThemedResource(resourceAnalyIconSvg)
+
 	// AppTabContainer
 	AppTabContainer := container.NewAppTabs(
-		container.NewTabItemWithIcon("ICMP Ping", theme.ComputerIcon(), ICMPPingContainer(a, w)),
-		container.NewTabItemWithIcon("TCP Ping", theme.ComputerIcon(), TCPPingContainer(a, w)),
-		container.NewTabItemWithIcon("HTTP Ping", theme.ComputerIcon(), HTTPPingContainer(a, w)),
-		container.NewTabItemWithIcon("DNS Ping", theme.ComputerIcon(), DNSPingContainer(a, w)),
-		container.NewTabItemWithIcon("Result Analysis", theme.ComputerIcon(), ResultAnalysisContainer(a, w)),
+		container.NewTabItemWithIcon("ICMP Ping", icmpIcon, ICMPPingContainer(a, w)),
+		container.NewTabItemWithIcon("TCP Ping", tcpIcon, TCPPingContainer(a, w)),
+		container.NewTabItemWithIcon("HTTP Ping", httpIcon, HTTPPingContainer(a, w)),
+		container.NewTabItemWithIcon("DNS Ping", dnsIcon, DNSPingContainer(a, w)),
+		container.NewTabItemWithIcon("Result Analysis", analyIcon, ResultAnalysisContainer(a, w)),
 	)
 
 	AppTabContainer.SetTabLocation(container.TabLocationLeading) // left

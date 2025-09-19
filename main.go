591c591913685ee5304c13e742cc2c20e8e375c1
Dennis Jian <dennis.jian01@gmail.com>
2025-09-19 21:59:13 +1000

    20250919_09

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ var (
 )
 
 // app Version
-var appVersion string = "1.0.5"
+var appVersion string = "1.0.6"
 
 // Create a global cancelable context
 var appCtx, cancelFunc = context.WithCancel(context.Background())
